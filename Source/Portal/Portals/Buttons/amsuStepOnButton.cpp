a02ffe4b81dafac2a60db5c4a631976b52d645ba
AmaterasuFury <ibestovu4@gmail.com>
2024-09-09 13:40:18 -0400

    add The big step on button class

---
+++ Source/Portal/Portals/Buttons/amsuStepOnButton.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "amsuStepOnButton.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AamsuStepOnButton::AamsuStepOnButton()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+
+	MeshComponentButtonBase = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button Base Mesh"));
+	MeshComponentButtonBase->SetupAttachment(RootComponent);
+
+	MeshComponentButton = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Button Mesh"));
+	MeshComponentButton->SetupAttachment(RootComponent);
+
+	BoxOverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Overlap Comnponent"));
+	BoxOverlapComponent->SetupAttachment(RootComponent);
+}
+
+void AamsuStepOnButton::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AamsuStepOnButton::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	SetActorTickEnabled(true);
+
+	ButtonPressed.Execute();
+}
+
+void AamsuStepOnButton::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	SetActorTickEnabled(false);
+
+	ButtonReleased.Execute();
+}
+
+void AamsuStepOnButton::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/Portal/Portals/Buttons/amsuStepOnButton.h
@@ -0,0 +1,45 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "amsuStepOnButton.generated.h"
+
+class UBoxComponent;
+
+DECLARE_DELEGATE(FButtonPressedEvent)
+DECLARE_DELEGATE(FButtonReleasedEvent)
+
+UCLASS()
+class PORTAL_API AamsuStepOnButton : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AamsuStepOnButton();
+	
+	virtual void Tick(float DeltaTime) override;
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Button")
+	TObjectPtr<UStaticMeshComponent> MeshComponentButtonBase;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Button")
+	TObjectPtr<UStaticMeshComponent> MeshComponentButton;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Button")
+	TObjectPtr<UBoxComponent> BoxOverlapComponent;
+
+	FButtonPressedEvent ButtonPressed;
+	FButtonReleasedEvent ButtonReleased;
+	
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+};
