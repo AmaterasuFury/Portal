5b3720dfe6821f0f1dc709987fcb359ab1810a3f
AmaterasuFury <ibestovu4@gmail.com>
2024-09-09 00:21:17 -0400

    Add Portal class

---
+++ Source/Portal/Portals/amsuPortal.cpp
@@ -0,0 +1,44 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Portal/Portals/amsuPortal.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+AamsuPortal::AamsuPortal()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	PrimaryActorTick.bStartWithTickEnabled = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+
+	MeshComponentPortal = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Portal Mesh"));
+	MeshComponentPortal->SetupAttachment(RootComponent);
+
+	BoxOverlapComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("Box Overlap Comnponent"));
+	BoxOverlapComponent->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void AamsuPortal::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AamsuPortal::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
+void AamsuPortal::OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void AamsuPortal::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
---
+++ Source/Portal/Portals/amsuPortal.h
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "amsuPortal.generated.h"
+
+class UBoxComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class PORTAL_API AamsuPortal : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AamsuPortal();
+	virtual void Tick(float DeltaTime) override;
+	
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Portal1")
+	TObjectPtr<UStaticMeshComponent> MeshComponentPortal;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Portal1")
+	TObjectPtr<UBoxComponent> BoxOverlapComponent;
+
+private:
+
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+		int32 OtherBodyIndex);
+};
