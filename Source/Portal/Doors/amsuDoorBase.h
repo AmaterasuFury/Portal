8e5c24c43022319c9b837eb06addb3c0dcb44dcc
AmaterasuFury <ibestovu4@gmail.com>
2024-09-16 11:44:25 -0400

    add base door and sliding door classes

---
+++ Source/Portal/Doors/amsuDoorBase.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Portal/amsuInteractable.h"
+#include "amsuDoorBase.generated.h"
+
+UCLASS()
+class PORTAL_API AamsuDoorBase : public AActor, public IamsuInteractable
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AamsuDoorBase();
+	
+	virtual void Close();
+	virtual void Open();
+	
+	void Lock();
+	void UnLock();
+	void ToggleLock();
+
+	virtual void Interact_Implementation(AActor* InteractActor) override;
+	
+	UPROPERTY(EditAnywhere, Category = "Door Parameters")
+	/** Sets the initial door lock status */
+	bool bIsLocked = false;
+};
