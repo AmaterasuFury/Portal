eb0965d9cffba8fb6891c49d5d78677f7eb26acb
AmaterasuFury <ibestovu4@gmail.com>
2024-11-04 14:01:04 -0500

    add unfinished bridge

---
+++ Source/Portal/Generators/Bridge.cpp
@@ -0,0 +1,34 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Bridge.h"
+
+
+ABridge::ABridge()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root Component"));
+
+	MeshComponentBridgeGenerator = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Bridge Generator Mesh"));
+	MeshComponentBridgeGenerator->SetupAttachment(RootComponent);
+
+	MeshFirstBridgePart
+}
+
+
+void ABridge::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void ABridge::SpawnBridge(TObjectPtr<UStaticMeshComponent> OriginalMesh)
+{
+	if (!IsValid(OriginalMesh))
+	{
+		return;
+	}
+	
+}
+
---
+++ Source/Portal/Generators/Bridge.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Bridge.generated.h"
+
+UCLASS()
+class PORTAL_API ABridge : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	ABridge();
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Bridge Generator")
+	TObjectPtr<UStaticMeshComponent> MeshComponentBridgeGenerator;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "First BridgePart")
+	TObjectPtr<UStaticMeshComponent> MeshFirstBridgePart;
+
+	UPROPERTY(EditAnywhere)
+	float MaxBridgeLength = 1000.f;
+protected:
+	virtual void BeginPlay() override;
+	
+	void SpawnBridge(TObjectPtr<UStaticMeshComponent> OriginalMesh);
+
+	// todo make a linetrace to get distance and use the distance to spawn the bridge
+};
