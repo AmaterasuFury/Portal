ce4c8520e2e6f943c229fca7244676bf07080650
AmaterasuFury <ibestovu4@gmail.com>
2024-11-25 09:19:08 -0500

    Add DetectInteractionComponent (not tested yet)

---
+++ Source/Portal/Components/amsuIntreactionDetectComponent.cpp
@@ -0,0 +1,53 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "amsuIntreactionDetectComponent.h"
+
+#include "Portal/amsuInteractable.h"
+
+
+UamsuIntreactionDetectComponent::UamsuIntreactionDetectComponent()
+{
+	PrimaryComponentTick.bStartWithTickEnabled = false;
+}
+
+void UamsuIntreactionDetectComponent::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	
+}
+
+AActor* UamsuIntreactionDetectComponent::DetectInteractable()
+{
+	const APlayerController* OwningController = Cast<APlayerController>(GetOwner());
+	if (!ensure(IsValid(OwningController)))
+	{
+		return nullptr;
+	}
+
+	FHitResult HitResult;
+	FCollisionQueryParams CollisionQueryParams;
+	CollisionQueryParams.AddIgnoredActor(GetOwner());
+
+	FVector ViewLocation = FVector::ZeroVector;
+	FRotator ViewRotation = FRotator::ZeroRotator;
+
+	OwningController->GetPlayerViewPoint(ViewLocation, ViewRotation);
+	const FVector TraceEnd = ViewLocation + ViewRotation.Vector() * CheckDistance;
+	
+	
+	OwningController->GetWorld()->LineTraceSingleByChannel(HitResult,ViewLocation, TraceEnd, ECC_Visibility, CollisionQueryParams);
+
+	AActor* HitActor = HitResult.GetActor();
+	if (HitActor)
+	{
+		 return HitActor->Implements<UamsuInteractable>() ? HitActor : nullptr;
+	}
+	else
+	{
+		return nullptr;
+	}
+}
+
+
---
+++ Source/Portal/Components/amsuIntreactionDetectComponent.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "amsuIntreactionDetectComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class PORTAL_API UamsuIntreactionDetectComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UamsuIntreactionDetectComponent();
+
+	UPROPERTY(EditAnywhere, Category = "Interaction Detection Component")
+	float CheckDistance = 200.f;
+
+/** Returns nullptr if no object implementing IamsuInteractable is found within the specified distance */
+	AActor* DetectInteractable();  
+	
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+};
