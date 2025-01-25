1083428af1ebbbd6d2bca5f6ee9694dc2f8c27dc
AmaterasuFury <ibestovu4@gmail.com>
2025-01-25 01:41:47 -0500

    Add Turret (Add functions to detect actors in the FOV)

---
+++ Source/Portal/PickUps/AamsuTurret.cpp
@@ -0,0 +1,89 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AamsuTurret.h"
+#include "Engine/OverlapResult.h"
+
+
+AamsuTurret::AamsuTurret()
+{
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+void AamsuTurret::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+/** Notice: the function checks the actors by their location, if the actor is big, the function might not detect it.
+ * If u want to make this function more accurate u would have to implement a logic to additionally trace a linetrace to actors angle, but it would cost you more of the performance, of course */
+TArray<AActor*> AamsuTurret::FOVActorsCheck(float InCheckDistance, ECollisionChannel InCollisionChannel) const
+{ 
+	const TArray<AActor*> AllActorsInRadius = GetAllActorsInRadius(InCheckDistance, InCollisionChannel);
+	TArray<AActor*> ActorsInFOV;
+
+	const FVector OwnerPosition = GetOwner()->GetActorLocation();
+	const FVector NormalizedOwnerForward = GetOwner()->GetActorForwardVector();
+	
+	TArray<FVector> NormalizedDirectionToTargets;
+	
+	for (const auto ActorInRadius : AllActorsInRadius)
+	{
+		if (IsValid(ActorInRadius))
+		{
+			const FVector NormalizedDirectionToTarget = (ActorInRadius->GetActorLocation() - OwnerPosition).GetSafeNormal();
+
+			const float DotProduct = FVector::DotProduct(NormalizedOwnerForward, NormalizedDirectionToTarget);
+			
+			const float Threshold = FMath::Cos(FMath::DegreesToRadians(FOVAngle / 2));
+			
+			if (DotProduct >= Threshold && !IsActorCovered(OwnerPosition, ActorInRadius)) 
+			{
+				ActorsInFOV.Add(ActorInRadius);
+			}
+		}
+	}
+	
+	return ActorsInFOV;
+}
+
+TArray<AActor*> AamsuTurret::GetAllActorsInRadius(float InCheckDistance, ECollisionChannel InCollisionChannel) const
+{
+	TArray<AActor*> FoundActors;
+	TArray<FOverlapResult> OutOverlaps;
+	FCollisionQueryParams FCollisionQueryParams;
+	
+	TArray<AActor*> IgnoredActors;
+	IgnoredActors.Add(GetOwner());
+	
+	FCollisionQueryParams.AddIgnoredActors(IgnoredActors);
+	
+	GetWorld()->OverlapMultiByChannel(OutOverlaps, GetOwner()->GetActorLocation(), FQuat::Identity, InCollisionChannel,
+		FCollisionShape::MakeSphere(InCheckDistance), FCollisionQueryParams);
+	
+
+	
+	
+	for (const FOverlapResult& OutOverlap : OutOverlaps)
+	{
+		if (!IsValid(OutOverlap.GetActor()))
+		{
+			continue;
+		}
+		FoundActors.Add(OutOverlap.GetActor());
+	}
+	
+	return FoundActors;
+}
+
+bool AamsuTurret::IsActorCovered(FVector OwnerPosition, AActor* TargetActor, ECollisionChannel InCollisionChannel) const
+{
+	FHitResult HitResult;
+	FCollisionQueryParams FCollisionQueryParams;
+
+	FCollisionQueryParams.AddIgnoredActor(GetOwner());
+	
+	GetWorld()->LineTraceSingleByChannel(HitResult, OwnerPosition, TargetActor->GetActorLocation(), InCollisionChannel, FCollisionQueryParams );
+	
+	return HitResult.GetActor() != TargetActor;
+}
---
+++ Source/Portal/PickUps/AamsuTurret.h
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AamsuPickUpObjectsBase.h"
+#include "AamsuTurret.generated.h"
+
+
+UCLASS()
+class PORTAL_API AamsuTurret : public AamsuPickUpObjectsBase
+{
+	GENERATED_BODY()
+
+public:
+	AamsuTurret();
+	
+	virtual void Tick(float DeltaTime) override;
+
+	UPROPERTY(EditAnywhere, Category = "Aimed Actor Component Parameters")
+	/** Recommended value is about 90 degrees */
+	float FOVAngle = 90.f;
+	
+	/** Gets all actors in the FOV, of the owner. Notice: the function checks the actors by their location, if the actor is big, the function might not detect it */
+	TArray<AActor*> FOVActorsCheck(float InCheckDistance, ECollisionChannel InCollisionChannel = ECC_Visibility) const;
+
+	TArray<AActor*> GetAllActorsInRadius(float InCheckDistance, ECollisionChannel InCollisionChannel = ECC_Visibility) const;
+
+	/** Checks if an actor is being Out Of Sight by doing LineTraceSingleByChannel */
+	bool IsActorCovered(FVector OwnerPosition, AActor* TargetActor, ECollisionChannel InCollisionChannel = ECC_Visibility) const;
+};
